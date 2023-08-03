16251976859f74bbc39c9c2c1610d4392e0c8e84
Elena Tonini <e.tonini002@studenti.unibs.it>
2023-08-03 17:34:55 +0200

    generate grid, add obstacles, generate pseudo-random routes for existing agents with respective cost

--- main.py
+++ main.py
@@ -1,52 +1,88 @@
+import math
 import random
 
-from Matrix import Matrix
+from matrix import Matrix
 import configparser
+import gen_hilbert_curve as ghc
+
+
+def generate_instance():
+    # instead of saving the adjacency matrix, I save only the list of non-zero elements of the matrix and check every
+    # time I have to make a move if the cell is empty. This way I save a lot of memory, especially for large grids with
+    # few non-zero elements.
+    grid = Matrix(NROWS, NCOLS)
+    n_obstacles = NOBSTACLES
+    random.seed(SEED)
+
+    offset_x = random.randint(0, NROWS - 1)
+    offset_y = random.randint(0, NCOLS - 1)
+    for x, y in ghc.gilbert2d(NROWS, NCOLS):  # using the generalized Hilbert curve to fill the grid with obstacles
+        if random.random() < AGGLOMERATION_FACTOR:
+            grid.add((x + offset_x) % NCOLS, (y + offset_y) % NROWS)
+            n_obstacles -= 1
+            if n_obstacles == 0:
+                break
+
+    # To guarantee that the agglomeration factor is exactly the one specified in the parameters.ini file, we'd need to
+    # add obstacles until the agglomeration factor is reached. However, this would contradict the requirement for a
+    # specific number of obstacles. Therefore, we add obstacles until the number is equal to the one specified in the
+    # parameters.ini file, and consider the agglomeration factor as a "secondary" parameter. This means that the
+    # agglomeration factor will be slightly lower than the required value.
+    # Specifically, the agglomeration factor will be correct for the cells of the grid that are internal to the
+    # clusters, while it will be lower than the required value for the cells that are on the border of the clusters.
+    # to prove that the agglomeration factor is correct if no constraint is added to the number of obstacles, remove the
+    # if n_obstacles == 0: break statement and uncomment the following lines:
+    # print(grid.calc_agg_fac())
+    # print(AGGLOMERATION_FACTOR)
+    # print(grid)
+
+    moves = dict()
+    costs = [0 for _ in range(NAGENTS)]
+    for t in range(PI_LENGTH):
+        moves[t] = dict()
+        for i in range(NAGENTS):
+            if t == 0:  # initial position
+                pos = (random.randint(0, NROWS - 1), random.randint(0, NCOLS - 1))
+
+                while grid.get(pos[0], pos[1]) or pos in moves[t].values():
+                    # if the position is occupied by an obstacle or by another agent, choose another one
+                    pos = (random.randint(0, NROWS - 1), random.randint(0, NCOLS - 1))
+                moves[t][i] = pos
+            else:
+                next_move = random.choice(
+                    grid.get_neighbours(moves[t - 1][i][0], moves[t - 1][i][1]) + [moves[t - 1][i]])
+
+                while next_move in moves[t].values() or grid.get(next_move[0], next_move[1]):
+                    # if the position is occupied by an obstacle or by another agent choose another one
+                    next_move = random.choice(
+                        grid.get_neighbours(moves[t - 1][i][0], moves[t - 1][i][1]) + [moves[t - 1][i]])
+                moves[t][i] = next_move
+                if grid.is_adjacent_cell(moves[t - 1][i][0], moves[t - 1][i][1], moves[t][i][0], moves[t][i][1]):
+                    costs[i] += 1
+                elif grid.is_diagonal_cell(moves[t - 1][i][0], moves[t - 1][i][1], moves[t][i][0], moves[t][i][1]):
+                    costs[i] += math.sqrt(2)
+
+    return grid, moves, costs
+
 
 if __name__ == '__main__':
-
+    # read parameters from .ini file
     config = configparser.ConfigParser()
     config.read('parameters.ini')
 
-    SEED = int(config['GENERATION']['SEED'])
+    SEED = int(config['GENERATION']['SEED'])  # for reproducibility
     NROWS = int(config['GENERATION']['NROWS'])
     NCOLS = int(config['GENERATION']['NCOLS'])
     NOBSTACLES = int(config['GENERATION']['NOBSTACLES'])
     AGGLOMERATION_FACTOR = float(config['GENERATION']['AGGLOMERATION_FACTOR'])
 
-    # instead of saving the adjacency matrix, I save only the list of non-zero elements of the matrix and check every
-    # time I have to make a move if the cell is empty. This way I save a lot of memory, especially for large grids with
-    # few non-zero elements.
+    PI_LENGTH = int(config['EXISTING_AGENTS']['PI_LENGTH'])  # constant length of the Pi route of the existing agents
+    NAGENTS = int(config['EXISTING_AGENTS']['NAGENTS'])
 
-    grid = Matrix(NROWS, NCOLS)
+    MAX = int(config['ENTRY_AGENT']['MAX'])  # maximum length of the route of the entry agent
+    INITX = int(config['ENTRY_AGENT']['INITX'])  # initial x position of the entry agent
+    INITY = int(config['ENTRY_AGENT']['INITY'])  # initial y position of the entry agent
+    GOALX = int(config['ENTRY_AGENT']['GOALX'])  # goal x position of the entry agent
+    GOALY = int(config['ENTRY_AGENT']['GOALY'])  # goal y position of the entry agent
 
-    n_obstacles = NOBSTACLES
-    pending = []
-    random.seed(SEED)  # for reproducibility
-    while n_obstacles > 0:
-        pending.append((random.randint(0, NROWS - 1), random.randint(0, NCOLS - 1)))
-        while len(pending) > 0:
-            i, j = pending.pop(0)  # last element
-            if (i, j) not in grid:
-                grid.add(i, j)
-                n_obstacles -= 1
-                if n_obstacles == 0:
-                    break
-                for l, k in grid.neighbours(i, j):
-                    if random.random() < AGGLOMERATION_FACTOR / 2.5 and (l, k) not in grid:
-                        # TODO: 2.5 is a magic number
-                        pending.append((l, k))
-
-    print(grid)
-    print(grid.calc_agg_fac())
-
-    # Per computare gli ostacoli, ho un fattore di agglomerazione che mi dice quanti ostacoli ci sono (%) in una certa
-    # area.
-    # Finché il numero di ostacoli è inferiore a quello richiesto:
-    # Per ogni cella, calcolo la probabilità che sia osatcolo e - se è un ostacolo - faccio sì che ogni cella
-    # adiacente sia un ostacolo con probabilità "fattore di agglomerazione". Una volta che una cella viene impostata
-    # come ostacolo, non può più essere modificata. Le celle rimaste libere, anche se già visitate, possono comunque
-    # essere trasformate in ostacoli più avanti durante l'esecuzione.
-
-    # se alla fine della generazione degli ostacoli, il numero di ostacoli è inferiore a quello richiesto, allora
-    # ricomincio da un'altra cella (vuota) scelta casualmente e ripeto il procedimento.
+    grid, moves, costs = generate_instance()
---
+++ matrix.py
@@ -0,0 +1,87 @@
+class Matrix:
+    """A sparse matrix of size height*width, where the matrix is represented as a dictionary of dictionaries."""
+
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.rows = dict()
+
+    def add(self, row, col, value=True):
+        """Adds the given element at the given position."""
+        assert (row < self.height)
+        assert (col < self.width)
+        if row not in self.rows:
+            self.rows[row] = dict()
+        self.rows[row][col] = value
+
+    def remove(self, row, col):  # TODO: check if this method is needed
+        """Removes the element at the given position, if it exists."""
+        assert (row < self.height)
+        assert (col < self.width)
+        if row in self.rows:
+            if col in self.rows[row]:
+                del self.rows[row][col]
+
+    def get(self, row, col):
+        """Returns the value of the element at the given position, or None if the element is not found."""
+        assert (row < self.height)
+        assert (col < self.width)
+        return self.rows.get(row, dict()).get(col, None)
+
+    def exists(self, row, col):
+        """Returns True if the element at the given position exists (i.e. is an obstacle), False otherwise."""
+        return self.get(row, col) is not None
+
+    def neighbours(self, row, col):
+        """Generator of neighbours of the element at the given position."""
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if i == 0 and j == 0:
+                    continue
+                if (row + i) < 0 or (row + i) >= self.height or (col + j) < 0 or (col + j) >= self.width:
+                    continue
+                yield row + i, col + j
+
+    def get_neighbours(self, row, col):
+        """Returns the list of neighbours of the element at the given position."""
+        return list(self.neighbours(row, col))
+
+    def calc_agg_fac(self):
+        """Returns the aggregation factor of the matrix."""
+        total = 0
+        valid = 0
+        for i in range(0, self.height):
+            for j in range(0, self.width):
+                if self.exists(i, j):
+                    for l, k in self.neighbours(i, j):
+                        if self.exists(l, k):
+                            valid += 1
+                        total += 1
+
+        return valid / total
+
+    def is_adjacent_cell(self, row1, col1, row2, col2):
+        """Returns True if the two given positions are adjacent and free from obstacles, False otherwise."""
+        return (not self.exists(row2, col2)) and (
+                (row1 == row2 and abs(col1 - col2) == 1) or (col1 == col2 and abs(row1 - row2) == 1))
+
+    def is_diagonal_cell(self, row1, col1, row2, col2):
+        """Returns True if the two given positions are diagonal and free from obstacles, False otherwise."""
+        return (not self.exists(row2, col2)) and (abs(row1 - row2) == 1 and abs(col1 - col2) == 1)
+
+    def __str__(self):
+        ret = ""
+        for row in range(self.height):
+            for col in range(self.width):
+                ret += 'X' if self.exists(row, col) else '.'
+            ret += '\n'
+        return ret
+
+    def __set__(self, instance, value: tuple):
+        self.add(*value)
+
+    def __getitem__(self, item: tuple):
+        return self.get(*item)
+
+    def __contains__(self, item: tuple):
+        return self.exists(*item)
